8b72cb2b2b96cad4f6f7a60a076cbbadc153c9a2
haritarun <tarunbommana798@gmail.com>
2026-04-22 11:29:05 +0530

    ..

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>Hello</h1>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./Pages/Register.jsx";
+import Login from "./Pages/Login.jsx";
+import Dashboard from "./Pages/Dashboard.jsx";
 
-export default App;
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
